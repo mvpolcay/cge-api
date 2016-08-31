2209cb1c5f9a8360f78972de15e0a02ecf24d43b
mvpolcay <olcaymhmmet@gmail.com>
2016-08-31 09:51:50 +0300

    Add files via upload

---
+++ apiCGE/Models/CategoryVM.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace apiCGE.Models
+{
+    public class CategoryVM
+    {
+        public int ID { get; set; }
+        public string  CategoryName { get; set; }
+        public string  Description { get; set; }
+    }
+}
---
+++ apiCGE/Models/EmployeeVM.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace apiCGE.Models
+{
+    public class EmployeeVM
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Title { get; set; }
+        public string TitleOfCourtsey { get; set; }
+        public string Phone { get; set; }
+
+    }
+}
